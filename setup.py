8a49bed8813e7cf4039ca733fcb3e0b3da8cb54b
Primož Godec <p.godec9@gmail.com>
2019-03-18 14:33:49 +0100

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ndf',
-    version='0.1',
+    version='0.1.1',
     author="Primoz Godec",
     author_email="primoz492@gmail.com",
     description="NumPy based deep learning framework",
